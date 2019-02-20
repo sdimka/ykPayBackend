8cfa98e58d1269e017189f72297f374cb9c63efb
SD-from work <sd@work>
2019-02-20 13:06:45 +0300

    YK Get impl begin

--- src/main/java/com/springTest/model/Payment.java
+++ src/main/java/com/springTest/model/Payment.java
@@ -1,7 +1,11 @@
 package com.springTest.model;
 
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +19,8 @@ public class Payment {
     private String status;
     private boolean paid;
     private Amount amount;
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     private PaymentMethod payment_method;
 
     private String description;
@@ -23,6 +29,16 @@ public class Payment {
         //
         return String.format("\n id: %s \n Status: %s \n Value: %f %s \n Paid: %s \n Description: %s \n S/N: %s \n",
                 id, status, amount.getValue(), amount.getCurrency(), paid, description, payment_method.getId());
+    }
+
+    @JsonGetter("payment_method_data")
+    public PaymentMethod getpayment_method_data() {
+        return payment_method;
+    }
+
+    @JsonSetter("payment_method")
+    public void setPayment_method(PaymentMethod payment_method) {
+        this.payment_method = payment_method;
     }
 
     // -------------------------------------------
